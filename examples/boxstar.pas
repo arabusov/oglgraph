e409b88ce5fafc8baf01c159d42c9e2554db6005
Andrei Rabusov <a.rabusov@tum.de>
2023-05-16 22:38:12 +0200

    [examples] improve boxstar example

--- examples/boxstar.pas
+++ examples/boxstar.pas
@@ -15,25 +15,26 @@ begin
     until graphkeypressed;
 end;
 
-procedure star(x, y, r: integer);
+procedure star(x, y, r, c: integer);
 var
-    r2: integer;
+    r2, i: integer;
+    ph: real;
 begin
+    ph := 72./180.*pi;
     if r > 1 then
     begin
         r2 := r div 2;
-        star(x - r, y + r, r2);
-        star(x + r, y + r, r2);
-        star(x - r, y - r, r2);
-        star(x + r, y - r, r2);
+        for i := 0 to 4 do
+            star(round(x + cos(i*ph+c*ph/2)*r), round(y + sin(i*ph+c*ph/2)*r), r2, c+1);
+        setcolor(c);
+        {circle(x, y, r2);}
         rectangle(x - r2, y - r2, x + r2, y + r2);
     end;
 end;
 
 begin
     initgraph(gr_driver, gr_mode, ' ');
-    setcolor(Cyan);
-    star(getmaxy div 2, getmaxy div 2, getmaxy div 4);
+    star(getmaxy div 2, getmaxy div 2, getmaxy div 4, 1);
     stop;
     closegraph
 end.
